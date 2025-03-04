3d64888d0372a2193e2e189b109927db71f06681
Fabian Scheler <fscheler@gmx.de>
2025-03-04 15:29:22 +0100

    ggBar3

--- R/ggmacro.R
+++ R/ggmacro.R
@@ -411,3 +411,29 @@ bdhw<-
     dw <- data.table::dcast(as.data.table(df), date ~ id, value.var = mnem)
     return(dw)
   }
+
+
+ggbaRfix<-
+  function (df, title = "Title", subtitle = "Subtitle", xcap = "",
+            ycap = "", name1 = "Portfolio", perc = T, col_aq2 = c("#04103b",
+                                                                  "#dd0400"), fredr_key = NULL)
+  {
+    library(ggplot2)
+    names(df) <- c("date", "asset")
+    #df$date <- as.Date(df$date)
+    cols <- setNames(c(col_aq2[1]), c(name1))
+    p <- ggplot(data = df, aes(x = df$date, y = df$asset))
+
+    p <- p + geom_col(size = 0.8, fill = col_aq2[1]) + theme_aq_black(base_size = 24) +
+      labs(color = "") + labs(title = title, subtitle = subtitle,
+                              x = xcap) + labs(caption = "") + theme(legend.position = "none",
+                                                                     legend.margin = margin(-20, -20, -20, -20), legend.box.margin = margin(0,
+                                                                                                                                            0, 0, 0)) + guides(colour = guide_legend(nrow = 1))
+    if (perc == TRUE) {
+      p <- p + scale_y_continuous(labels = scales::percent,
+                                  expand = c(0, 0))
+    }
+    p <- p + ylab(ycap) + theme(plot.margin = margin(5, 5, 5,
+                                                     5))
+    return(p)
+  }
