7344d81ecf92b855837986a6b13eee9144740610
Fabian Scheler <fscheler@gmx.de>
2026-04-11 11:08:16 +0200

    bug fix

--- R/dbhelpeR.R
+++ R/dbhelpeR.R
@@ -16,3 +16,43 @@ fastwRiter<-function(conn,name,value,append=T,overwrite=F,row.names=F)
   end<-Sys.time()
   end-start 
 }
+
+
+chunkwRiter<-function(pool,name,value,overwrite=F,append=T)
+{
+  start<-Sys.time()
+  
+  if(overwrite==T | append==F)
+  {
+    dbWriteTable(pool,name,head(value,0),overwrite=T,row.names= row.names)
+  }
+  
+  conn <- poolCheckout(pool)
+  
+  chunk_size <- 10000
+  chunks <- split(value, (seq_len(nrow(value)) - 1) %/% chunk_size)
+  
+  dbBegin(conn)
+  
+  for (chunk in chunks) {
+    values_sql <- paste(
+      apply(chunk, 1, function(row) {
+        paste0("(", paste(DBI::dbQuoteLiteral(conn, row), collapse = ","), ")")
+      }),
+      collapse = ","
+    )
+    
+    sql <- paste0(
+      paste0("INSERT INTO ",name," VALUES "),
+      values_sql
+    )
+    
+    dbExecute(conn, sql)
+  }
+  
+  dbCommit(conn)
+  poolReturn(conn)
+  
+  end<-Sys.time()
+  end-start  
+}
